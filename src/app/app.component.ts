8313d49419d27e554484c18af94d91bdeffa2ef1
Julian <julian.steigerwald@googlemail.com>
2025-03-09 16:29:52 +0100

    extract characters into own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
-import {Character, CharacterSheetComponent} from './character-sheet/character-sheet.component';
+import {CharacterSheetComponent} from './character-sheet/character-sheet.component';
 import {CountdownComponent} from './countdown/countdown.component';
 import {XDividerComponent} from './x-divider/x-divider.component';
+import {julz, lucy} from './model/characters';
 
 @Component({
   selector: 'app-root',
@@ -14,29 +15,7 @@ import {XDividerComponent} from './x-divider/x-divider.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wedding';
-
-  lucy: Character = {
-    name: 'Lucy',
-    race: 'Human',
-    class: 'ÄWÄWÄ',
-    level: 26,
-    attributes: {str: 10, dex: 10, cha: 10, wis: 10, int: 10, con: 10},
-    backstory: 'ÄWÄWÄ',
-    abilities: ['eepy +10', 'hangry +2'],
-    equipment: ['adhs toys'],
-    imagePath: 'default.jpg'
-  }
-
-  julz: Character = {
-    name: 'Lucy',
-    race: 'Human',
-    class: 'ÄWÄWÄ',
-    level: 26,
-    attributes: {str: 10, dex: 10, cha: 10, wis: 10, int: 10, con: 10},
-    backstory: 'ÄWÄWÄ',
-    abilities: ['eepy +10', 'hangry +2'],
-    equipment: ['adhs toys'],
-    imagePath: 'default.jpg'
-  }
+  protected readonly lucy = lucy;
+  protected readonly julz = julz;
+  protected readonly title = 'wedding';
 }
---
+++ src/app/model/characters.ts
@@ -0,0 +1,25 @@
+import {Character} from '../character-sheet/character-sheet.component';
+
+export const lucy: Character = {
+  name: 'Lucy',
+  race: 'Human',
+  class: 'ÄWÄWÄ',
+  level: 26,
+  attributes: {str: 10, dex: 10, cha: 10, wis: 10, int: 10, con: 10},
+  backstory: 'ÄWÄWÄ',
+  abilities: ['eepy +10', 'hangry +2'],
+  equipment: ['adhs toys'],
+  imagePath: 'default.jpg'
+}
+
+export const julz: Character = {
+  name: 'Lucy',
+  race: 'Human',
+  class: 'ÄWÄWÄ',
+  level: 26,
+  attributes: {str: 10, dex: 10, cha: 10, wis: 10, int: 10, con: 10},
+  backstory: 'ÄWÄWÄ',
+  abilities: ['eepy +10', 'hangry +2'],
+  equipment: ['adhs toys'],
+  imagePath: 'default.jpg'
+}
