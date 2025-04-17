bdc70788ad71dc4efeb090b5d03311dd986dace1
Julian <julian.steigerwald@googlemail.com>
2025-04-17 18:21:29 +0200

    use router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
 import {Component} from '@angular/core';
-import {CharacterSheetComponent} from './character-sheet/character-sheet.component';
-import {CountdownComponent} from './countdown/countdown.component';
-import {XDividerComponent} from './x-divider/x-divider.component';
-import {julz, lucy} from './model/characters';
-import {MapsContainerComponent} from './maps-container/maps-container.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    CharacterSheetComponent,
-    CountdownComponent,
-    XDividerComponent,
-    MapsContainerComponent
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  protected readonly lucy = lucy;
-  protected readonly julz = julz;
-  protected readonly title = 'wedding';
 }
