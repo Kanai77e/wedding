007a1b795397c882cbe1dd8e4688cdbf5c0a5c3a
Julian <julian.steigerwald@googlemail.com>
2025-05-21 14:11:29 +0200

    cleanup css

--- src/app/character-sheet/character-sheet.component.ts
+++ src/app/character-sheet/character-sheet.component.ts
@@ -1,25 +1,5 @@
 import {Component, input} from '@angular/core';
-
-export interface Character {
-  name: string;
-  race: string;
-  class: string;
-  level: number;
-  attributes: Attributes;
-  backstory: string;
-  abilities: string[];
-  equipment: string[];
-  imagePath: string;
-}
-
-export interface Attributes {
-  str: number;
-  dex: number;
-  cha: number;
-  wis: number;
-  int: number;
-  con: number;
-}
+import {Character} from '../model/characters';
 
 @Component({
   selector: 'app-character-sheet',
