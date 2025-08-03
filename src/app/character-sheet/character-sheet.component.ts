695fd965e918116981c1b6557fc01002416d7488
Julian <julian.steigerwald@googlemail.com>
2025-08-03 20:22:25 +0200

    hide attributes

--- src/app/character-sheet/character-sheet.component.ts
+++ src/app/character-sheet/character-sheet.component.ts
@@ -10,5 +10,5 @@ import {Character} from '../model/characters';
 
 export class CharacterSheetComponent {
   character = input.required<Character>()
-  showAttributes = input<boolean>(true);
+  showAttributes = input<boolean>(false);
 }
